8c50b9bee8083767386881b2641bf144f5d90a8d
tanveer-98 <tnvrahmed98@gmail.com>
2023-03-06 07:33:10 +0530

    fix : overflow fix

--- src/components/Parallax/index.tsx
+++ src/components/Parallax/index.tsx
@@ -1,25 +1,9 @@
 import React from 'react'
-import { MouseParallaxContainer, MouseParallaxChild } from "react-parallax-mouse";
-const ParallaxComponent = () => {
-  
-  function parallax (event : any ){
-    this.querySelectorAll()
-  } 
-  document.addEventListener("mousemove" , parallax);
-  
-  
+
+const index = () => {
   return (
-    <section className="parallax">
-      <MouseParallaxContainer globalFactorX={1} globalFactorY={1}>
-  <MouseParallaxChild factorX={1} factorY={1}>
-    <img className="c-1" src="circle.png" alt="" />
-  </MouseParallaxChild>
-  <MouseParallaxChild factorX={1} factorY={1}>
-    <img className="c-2"
-    src="circle.png" alt="" />
-  </MouseParallaxChild>
-</MouseParallaxContainer></section>
+    <div>index</div>
   )
 }
 
-export default ParallaxComponent
+export default index
