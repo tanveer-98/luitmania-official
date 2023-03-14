e881b500a83f639a2ae54e4b829b535c362a5688
tanveer-98 <tnvrahmed98@gmail.com>
2023-03-14 23:54:47 +0530

    linkedin

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -80,7 +80,7 @@ const Footer = () => {
       </a> */}
 
             <a
-              href="#!"
+              href="https://www.linkedin.com/company/luitomania-software-develoment-company/"
               type="button"
               className="m-1 h-9 w-9 rounded-full border-2 border-white uppercase leading-normal text-white transition duration-150 ease-in-out hover:bg-black hover:bg-opacity-5 focus:outline-none focus:ring-0"
               data-te-ripple-init
